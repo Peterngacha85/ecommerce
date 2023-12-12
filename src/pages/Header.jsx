a569713b7c34b9bc57695313cb0b8e9511261951
Peterngacha85 <peterngacha85@gmail.com>
2023-12-12 18:07:20 +0300

    under development

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import '../styles/header.css'
+import React from 'react';
+import '../styles/header.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 
 const Header = () => {
   return (
     <div className='header'>
-        <div className='logo'>
-            <h5>LOGO</h5>
-        </div>
-    <div className='shop-cart'>
-    <h5>SHOP</h5>
-      <h5>CART</h5>
+      <div className='logo'>
+        <h5>LOGO</h5>
+      </div>
+      <div className='shop-cart'>
+        <h5>SHOP</h5>
+        <h5><i className="bi bi-cart-fill"></i></h5>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
